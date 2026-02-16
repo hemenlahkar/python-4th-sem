aa4527349629357bc3a422fe7a745bd5de1217f8
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-02-16 23:37:44 +0530

    minor performance improvement

--- examples/01_helloWorld.py
+++ examples/01_helloWorld.py
@@ -3,14 +3,12 @@ str = "Hello World!"
 alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ!"
 
 print("\033[1;31m", end="")
-for i in range(1, len(str) + 1):
-    print("\r" + str[:i], end="")
-    if i < len(str):
-        for j in alphabet:
-            t.sleep(0.05)
-            print(j, end="")
-            if j != str[i]:
-                print("\b", end="")
-            else:
-                break
+for i in range(len(str)):
+    for j in alphabet:
+        t.sleep(0.05)
+        print(j, end="")
+        if j != str[i]:
+            print("\b", end="")
+        else:
+            break
 print("\033[0;0m")
