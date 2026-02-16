05861ec0bd01a449033a0bb87c5aeceb46c0975b
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-02-16 20:28:08 +0530

    add smallest and greatest of three numbers

---
+++ examples/03_minMax3.py
@@ -0,0 +1,26 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+largest = smallest = 0
+
+if a > b and a > c:
+    largest = a
+    if b < c:
+        smallest = b
+    else:
+        smallest = c
+elif b > c:
+    largest = b
+    if a < c :
+        smallest = a
+    else:
+        smallest = c
+else:
+    largest = c
+    if b < a:
+        smallest = b
+    else:
+        smallest = a
+        
+print(f"Smallest: {smallest}\nLargest: {largest}")
