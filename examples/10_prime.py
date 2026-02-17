1f7e4c092c3e8003ee6c1eb993e5514ef57389ac
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-02-17 19:15:32 +0530

    add 100 prime numbers

--- examples/10_prime.py
+++ examples/10_prime.py
@@ -1,17 +1,15 @@
 def isPrime(n):
-    if n <= 1:
-        return False
-    elif n == 2:
+    if n == 2:
         return True
+    if n <= 1 or n % 2 == 0:
+        return False
     
     for i in range(3, n//2 + 1, 2):
         if n % i == 0:
             return False
     return True
 
-n = int(input("Enter a number: "))
 
-if isPrime(n):
-    print(f"{n} is a Prime Number.")
-else:
-    print(f"{n} is NOT a prime number!")
+for i in range(2, 100):
+    if isPrime(i):
+        print(f"{i:2d}", end=" ")
