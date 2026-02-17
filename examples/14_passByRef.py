8fdad799bdd98d580229232e65a75c73e208ab12
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-02-17 20:02:01 +0530

    add pass arguement by reference

---
+++ examples/14_passByRef.py
@@ -0,0 +1,7 @@
+def swap(lst):
+    lst[0], lst[1] = lst[1], lst[0]
+
+values = [5, 10]
+print(f"Before: x = {values[0]}, y = {values[1]}")
+swap(values)
+print(f"After: x = {values[0]}, y = {values[1]}")
