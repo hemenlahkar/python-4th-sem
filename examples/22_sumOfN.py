2be058285ac3d4c77afc2e82dd599581b3e13834
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-02-21 10:37:52 +0530

    add sum of n numbers

---
+++ examples/22_sumOfN.py
@@ -0,0 +1,5 @@
+# Write a program to find sum of n elements entered by the user.
+
+nums = [int(x) for x in input("Enter the numbers to be added: ").split()]
+
+print("Sum of the entered numbers =", sum(nums))
